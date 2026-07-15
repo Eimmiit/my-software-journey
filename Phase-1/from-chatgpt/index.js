ca2acbfa15db1c10fb4259a2e899fa7c73fa2541
Timilehin Orimisan <eiimmiit@gmail.com>
2026-07-15 09:17:05 +0100

    added index.js

---
+++ Phase-1/from-chatgpt/index.js
@@ -0,0 +1,37 @@
+// console.log('Hello World');
+
+// let name = prompt('What is your name oh dearie?');
+// alert("hello " + name);
+// console.log(name);
+
+
+// let userName = prompt("what is your Username");
+// console.log(`Welcome ${userName}`);
+
+// let favProgram = prompt("what is your favourite programming language");
+// console.log("Your favourite programming language is " + favProgram);
+
+// let firstNum = Number(prompt('come up with the first number'));
+// let secondNum = Number(prompt('come up with the second number'));
+// console.log(firstNum + secondNum);
+// console.log(firstNum - secondNum);
+// console.log(firstNum * secondNum);
+// console.log(firstNum ** secondNum);
+
+// let name = prompt("what is your name dear");
+// let age = Number(prompt("how old are you darling"));
+// let country = prompt("what country are you based in");
+// console.log(`My name is ${name}, I am ${age} years old and i live in ${country}`);
+
+// let birthYear = Number(prompt('What is your birth year'));
+// const currentYear = 2026;
+// let age = currentYear - birthYear;
+// console.log(age);
+
+// // Engineering challenge
+// let fullName = prompt('what is your full name');
+// let age = Number(prompt('age'));
+// let department = prompt('please enter your department');
+// let level = prompt('what level are you');
+// let cgpa = Number(prompt("what's is your cgpa"));
+// console.log(`====Student Registration Program.==== \n Name: ${fullName} \n Age: ${age} \n Department: ${department} \n Level: ${level} \n CGPA: ${cgpa}`); 
